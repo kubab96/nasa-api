748b08257108678eaa138a61268b8acd64bf9222
kubab96 <kubabaran12@gmail.com>
2021-08-22 17:34:01 +0200

    Home button

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 export default function Home(){
     return(
         <div>
-            <Link to ="/Apod">Apod</Link>
+            <Link className="button" to ="/Apod">Apod</Link>
         </div>
     )
 }
